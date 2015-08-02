28a4770cf1b1df7f3839da81878b267b25598b22
vicagbur <victorag@gmail.com>
2015-08-02 22:48:02 +0200

    autoload

--- routes/index.js
+++ routes/index.js
@@ -8,9 +8,13 @@ router.get('/', function(req, res) {
   res.render('index', { title: 'Quiz' });
 });
 
-router.get('/quizes',       quizController.index);
-router.get('/quizes/:quizId(\\d+)',       quizController.show);
-router.get('/quizes/:quizId(\\d+)/answer',       quizController.answer);
+//Autoload de comandos con :quizId
+router.param('quizId', quizController.load); //autoload :quizId
+
+
+router.get('/quizes',                       quizController.index);
+router.get('/quizes/:quizId(\\d+)',         quizController.show);
+router.get('/quizes/:quizId(\\d+)/answer',  quizController.answer);
 //router.get('/quizes/question', quizController.question);
 //router.get('/quizes/answer', quizController.answer);
 
